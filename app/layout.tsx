ae5dc5797c405037ab8e5e8278d2ba04917b9c6d
Felipe Ocampo Araujo <113953095+Elpipilo18@users.noreply.github.com>
2025-10-17 12:47:06 -0600

    Include Google Ads script in layout
    Added Google Ads script to the layout head.

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3083202631788622"
+     crossorigin="anonymous"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
